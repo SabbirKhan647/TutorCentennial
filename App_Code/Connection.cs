20811b494b58d1c8f58e83ef27fff7ecfc2d4afe
SabbirKhan647 <sabbirkhan647@gmail.com>
2014-03-20 12:17:30 -0400

    FirstChanges
    Initial Files

---
+++ App_Code/Connection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Security;
+
+/// <summary>
+/// Summary description for Connection
+/// </summary>
+public static class Connection
+{
+    public static SqlConnection connect()
+    {
+        SqlConnection con = new SqlConnection("Data Source=SABBIR\\SQLSERVER2012;Initial Catalog=TutorCentennial;Integrated Security=True");
+        return con;
+    }
+
+    public static int StudentID = 1;
+    public static string RoleName { get; set; }
+        public static string SecondTry { get; set; }  
+    //Yes    
+
+}
